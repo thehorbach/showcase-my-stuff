0a732f4d9c8b22cc2983826ce1a0c43f0bf5f540
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-04-03 22:36:16 +0200

    added facebook login

--- showcase-my-stuff/ViewController.swift
+++ showcase-my-stuff/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -18,6 +21,24 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func fbBtnPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                print("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                
+                print("Successfully logged in with Facebook. \(accessToken)")
+            }
+            
+        }
     }
 
 
