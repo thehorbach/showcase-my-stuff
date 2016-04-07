a79e0c584b6c7bc10603fd8245c3974fc78fa6f0
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-04-07 21:21:21 +0200

    added firebase urls for users and posts

--- showcase-my-stuff/DataService.swift
+++ showcase-my-stuff/DataService.swift
@@ -9,11 +9,23 @@
 import Foundation
 import Firebase
 
+let URL_BASE = "https://showcase-my-stuff.firebaseio.com"
+
 class DataService {
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://showcase-my-stuff.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
 
+    var REF_USERS: Firebase {
+        return _REF_USERS
+    }
+    
+    var REF_POSTS: Firebase {
+        return _REF_POSTS
+    }
+    
     var REF_BASE: Firebase {
         return _REF_BASE
     }
