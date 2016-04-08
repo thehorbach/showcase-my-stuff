a9d1c04591d7cbdb27e0d17a458d71e51da274bb
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-04-08 13:41:30 +0200

    parsed data from firebase

--- showcase-my-stuff/FeedVC.swift
+++ showcase-my-stuff/FeedVC.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +23,28 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
+            
+            self.posts = []
+            
+            if let snapshot = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
         })
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -32,6 +52,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 }
---
+++ showcase-my-stuff/Post.swift
@@ -0,0 +1,61 @@
+//
+//  Post.swift
+//  showcase-my-stuff
+//
+//  Created by Vyacheslav Horbach on 08/04/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imgUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imgUrl: String? {
+        return _imgUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var userName: String {
+        return _username
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(description: String, imageUrl: String?, username: String) {
+        self._postDescription = description
+        self._imgUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let likes = dictionary["likes"] as? Int {
+            self._likes = likes
+        }
+        
+        if let url = dictionary["imgUrl"] as? String {
+            self._imgUrl = url
+        }
+        
+        if let desc = dictionary["description"] as? String {
+            self._postDescription = desc
+        }
+        
+    }
+    
+}
