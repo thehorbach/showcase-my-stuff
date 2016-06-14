879809826b2bcd6f57710048a0c7cdb37fbed575
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-06-14 20:04:35 +0200

    finished fucking V1.0 of this app, shit took so long and descouraged me from coding

--- showcase-my-stuff/PostCell.swift
+++ showcase-my-stuff/PostCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -15,12 +16,20 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var showcaseImg: UIImageView!
     @IBOutlet weak var descriptionText: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
     
     var post: Post!
     var request: Request?
+    
+    var likeRef: Firebase!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        let tap = UITapGestureRecognizer(target: self, action: "likeTapped:")
+        tap.numberOfTapsRequired = 1
+        likeImage.addGestureRecognizer(tap)
+        likeImage.userInteractionEnabled = true
     }
     
     override func drawRect(rect: CGRect) {
@@ -32,6 +41,8 @@ class PostCell: UITableViewCell {
     
     func configureCell(post: Post, img: UIImage?) {
         self.post = post
+        
+        likeRef = DataService.ds.REF_USER_CURRENT.childByAppendingPath("likes").childByAppendingPath(post.postKey)
         
         self.descriptionText.text = post.postDescription
         self.likesLbl.text = "\(post.likes)"
@@ -53,5 +64,51 @@ class PostCell: UITableViewCell {
         } else {
             self.showcaseImg.hidden = false
         }
+        
+        likeRef.observeSingleEventOfType(.Value, withBlock: {snapshot in
+            
+            if let doesNotExist = snapshot.value as? NSNull {
+                //This means we have not liked this specific post
+                self.likeImage.image = UIImage(named: "heart-empty")
+            } else {
+                self.likeImage.image = UIImage(named: "heart-full")
+            }
+            
+        })
+    }
+    
+    
+    func likeTapped(sender: UITapGestureRecognizer) {
+        likeRef.observeSingleEventOfType(.Value, withBlock: {snapshot in
+            
+            if let doesNotExist = snapshot.value as? NSNull {
+                //This means we've just liked this specific post
+                self.likeImage.image = UIImage(named: "heart-full")
+                self.post.adjustLikes(true)
+                self.likeRef.setValue(true)
+            } else {
+                self.likeImage.image = UIImage(named: "heart-empty")
+                self.post.adjustLikes(false)
+                self.likeRef.removeValue()
+            }
+            
+        })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
